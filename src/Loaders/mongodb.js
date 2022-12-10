fe1af4640f25506021d0a4300ca1026403b63e53
Sami Safatli <samisafatli@live.com>
2022-12-10 16:18:08 -0300

    Add mongo setup

---
+++ src/Loaders/mongodb.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+ async function startDB() {
+    await mongoose.connect('mongodb+srv://admin:admin@clusterlongs.upraqsa.mongodb.net/test')
+}
+
+module.exports = startDB
