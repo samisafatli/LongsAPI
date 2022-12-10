2416165edee7cce8732760148fbe2a3ede066062
Sami Safatli <samisafatli@live.com>
2022-12-10 16:17:45 -0300

    Add mongo Schema

---
+++ src/Models/ONGsModels.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const OngSchema = new Schema({
+  id: ObjectId,
+  title: String,
+  description: String,
+  email: String,
+  category: String,
+});
+
+const OngModel = mongoose.model('ongs', OngSchema)
+
+module.exports = OngModel
